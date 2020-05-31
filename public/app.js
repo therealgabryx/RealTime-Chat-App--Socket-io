10976cc004cd898fa7550448c57b12a0bb2924ea
Gabriele Concli <gabriele.concli@gmail.com>
2020-05-31 13:47:58 +0200

    socket emit broadcast added - now displaying: (name) is typing..

--- public/app.js
+++ public/app.js
@@ -4,6 +4,7 @@ var socket = io.connect('http://localhost:4000')
 // Query DOM 
 
 var message = document.getElementById('message')
+var feedback = document.getElementById('feedback')
 var handle = document.getElementById('handle')
 var btn = document.getElementById('send')
 var output = document.getElementById('output')
@@ -16,7 +17,17 @@ btn.addEventListener('click', () => {
     })
 }) 
 
-// Listen for events 
-socket.on('chat', (data) => {
-    output.innerHTML += '<p><strong>' + data.handle + ': </strong>' + data.message + '</p>'
+message.addEventListener('keypress', () => {
+    socket.emit('typing', handle.value)
 })
+
+// Listen for events 
+socket.on('chat', (data) => { 
+    feedback.innerHTML = ''
+    feedback.value = ''
+    output.innerHTML += '<p><strong>' + data.handle + ': </strong>' + data.message + '</p>'
+}) 
+
+socket.on('typing', (data) => {
+    feedback.innerHTML =  '<p><em>' + data + ' is typing a message..</em></p>'
+}) 
